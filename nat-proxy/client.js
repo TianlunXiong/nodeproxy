be4f5389b476eb6a690debd637eb98d0106a15dd
tainlx <418219627@qq.com>
2021-04-05 00:23:37 +0800

    feat: 多路复用

--- nat-proxy/client.js
+++ nat-proxy/client.js
@@ -1,46 +1,46 @@
 const net = require('net')
 const config = require('./config.json')
 
-const remote_port = config.server.local_port;
+const remote_port = config.client.local_port;
 const remote_host = config.server.host;
+const data_port = config.server.data_port
 const working_port = config.client.working_port;
 
-const remote_socket = net.createConnection(remote_port, remote_host, () => {
+const controller_socket = net.createConnection(remote_port, remote_host, () => {
     console.log('正在连接穿透服务器...')
 })
 
-const working_socket = net.createConnection(working_port, '0.0.0.0', () => {
-    console.log('正在连接本地服务器...')
-})
+
 
 run()
 
 async function run () {
     await new Promise((resolve, reject) => {
         let remote_ok;
-        let app_ok;
-        remote_socket.on('connect', () => {
+        controller_socket.on('connect', () => {
             console.log('远程连接成功')
             remote_ok = true
             if (check()) resolve()
         })
-        
-        working_socket.on('connect', () => {
-            console.log('本地连接成功')
-            app_ok = true
-            if (check()) resolve()
-        })
 
         function check() {
-            if (remote_ok === true && app_ok === true) return true;
+            if (remote_ok === true) return true;
             return false;
         }
     })
-
-    remote_socket.pipe(working_socket)
-    working_socket.pipe(remote_socket)
 }
 
+controller_socket.on('data', (d) => {
+    const data_socket = net.createConnection(data_port, remote_host, () => {
+        console.log('数据通道已连接...')
+    })
+    data_socket.write(d)
+    const working_socket = net.createConnection(working_port, '0.0.0.0', () => {
+        console.log('正在连接应用服务器...')
+    })
+    data_socket.pipe(working_socket)
+    working_socket.pipe(data_socket)
+})
 
 
 
--- nat-proxy/server.js
+++ nat-proxy/server.js
@@ -2,39 +2,53 @@ const net = require('net')
 const config = require('./config.json')
 
 const public_port = config.server.public_port
-const local_port = config.server.local_port
+const local_port = config.client.local_port
+const data_port = config.server.data_port
 
-let local_socket;
+const publicSocketMap = new Map();
+const dataSocketMap = new Map();
+
+let controller_socket = new net.Socket()
 
 const public_server = net.createServer()
 
 public_server.on('connection', (public_socket) => {
-    if (local_socket) {
-        try {
-            public_socket.pipe(local_socket)
-            local_socket.pipe(public_socket)
-        } catch (e) {
-            console.error(e)
-        }
-    } else {
-        console.error('未能连接客户端')
-    }
+    const n = Math.random().toFixed(20);
+    controller_socket.write(n);
+    publicSocketMap.set(n, public_socket);
 })
 
 public_server.listen(public_port, '0.0.0.0', () => {
     console.log(`公网服务已开启，端口:${public_port}`)
 })
 
-const local_server = net.createServer((socket) => {
-    
-})
+const controller_server = net.createServer()
 
-local_server.on('connection', (s) => {
-    local_socket = s
-    console.log(`客户端已接入: ${s.remoteAddress}:${s.remotePort}`)
+controller_server.on('connection', (socket) => {
+    controller_socket = socket
+    console.log(`控制通道已接入: ${socket.remoteAddress}:${socket.remotePort}`)
 })
 
 
-local_server.listen(local_port, '0.0.0.0', () => {
-    console.log(`NAT穿透服务已开启，端口:${local_port}`)
+controller_server.listen(local_port, '0.0.0.0', () => {
+    console.log(`控制通道已开启，端口:${local_port}`)
 })
+
+const data_server = net.createServer()
+
+data_server.on('connection', (data_socket) => {
+    console.log(`数据通道已接入: ${data_socket.remoteAddress}:${data_socket.remotePort}`)
+    data_socket.on('data', (d) => {
+        const pub_socket = publicSocketMap.get(`${d}`)
+        if (pub_socket) {
+            pub_socket.pipe(data_socket)
+            data_socket.pipe(pub_socket)
+        }
+    })
+})
+
+data_server.listen(data_port, '0.0.0.0', () => {
+    console.log(`数据通道已开启，端口:${data_port}`)
+})
+
+
